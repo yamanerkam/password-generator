1cc978e99f5bdd0e2a5fe0a171f3e27033d84cd2
yamanerkam <erkamyaman35@gmail.com>
2025-01-02 22:40:03 +0300

    basic-random-number-generator-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'password-generator';
+  password : string = '';
+  digitsValue : number = 0;
+
+  getRandomDigits(quantity: number){
+      let result = '';
+      for (let i = 0; i < quantity; i++) {
+          result += Math.floor(Math.random() * 10); // Generates a random digit (0-9)
+      }
+      this.password = result;
+
+  }
+
+  
 }
